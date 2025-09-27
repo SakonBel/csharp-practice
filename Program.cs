466ecaf21b9d2a517f999b16a4543bd0d7629684
Sakon Plangklang <iamsabell@gmail.com>
2025-09-27 22:16:15 +0700

    Complete string challenges

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+// int n1 = 10;
+// string name = "Bel";
+// char letter = 'Z';
+// double n2 = 2.2;
+// Console.WriteLine(name, n1 + n2);
+// Console.WriteLine(letter);
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.StartsWith("goodbye"));
+Console.WriteLine(songLyrics.EndsWith("hello"));
--- obj/Debug/net9.0/practice.AssemblyInfo.cs
+++ obj/Debug/net9.0/practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb97dcaf20251ff94cee39a738f9630750de5cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
