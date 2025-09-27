98b4dec0a54c1aa38a8ee07a26d66fee75fb9c16
Sakon Plangklang <iamsabell@gmail.com>
2025-09-27 22:22:34 +0700

    Complete the number challenges

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@
 // double n2 = 2.2;
 // Console.WriteLine(name, n1 + n2);
 // Console.WriteLine(letter);
+
+// ------------- String Challenges ------------------
 string songLyrics = "You say goodbye, and I say hello";
 Console.WriteLine(songLyrics.StartsWith("goodbye"));
 Console.WriteLine(songLyrics.EndsWith("hello"));
+
+// ------------- Number Challenges ------------------
+double PI = Math.PI;
+double radius = 2.50;
+double circleArea = PI * (Math.Pow(radius, 2));
+Console.WriteLine(circleArea);
--- obj/Debug/net9.0/practice.AssemblyInfo.cs
+++ obj/Debug/net9.0/practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb97dcaf20251ff94cee39a738f9630750de5cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466ecaf21b9d2a517f999b16a4543bd0d7629684")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
